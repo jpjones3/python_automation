c7f991113ee7337a4b82a5f55aceeec2ee20f404
jpjones3 <jpjones3@hotmail.com>
2024-02-27 22:44:11 -0500

    Committing change via GitHub API!

--- work/env1/main.tf
+++ work/env1/main.tf
@@ -1,11 +1,23 @@
 resource "aws_s3_bucket1" "s3_bucket" {
   bucket = var.bucket_name
-  version = 0.3.10
+  version = "0.3.16"
   tags = var.tags
 }
 
 resource "aws_s3_bucket2" "s3_bucket" {
   bucket = var.bucket_name
-  version = 0.3.10
+  version = "0.3.16"
+  tags = var.tags
+}
+
+resource "aws_s3_bucket2" "s3_bucket" {
+  bucket = var.bucket_name
+  version = "0.3.16"
+  tags = var.tags
+}
+
+resource "aws_s3_bucket2" "s3_bucket" {
+  bucket = var.bucket_name
+  version = "0.3.16"
   tags = var.tags
 }
